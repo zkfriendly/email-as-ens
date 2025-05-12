762b0d52f6633d98830e3aadfe43fabeb2d0f9e3
zkfriendly <zkfriendly@proton.me>
2025-05-12 15:49:00 +0200

    feat: init ZKEmailRegistrar

---
+++ src/ZKEmailRegistrar.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ZKEmailUtils} from "@openzeppelin/community-contracts/utils/cryptography/ZKEmailUtils.sol";
+import {EmailAuthMsg} from "@zk-email/email-tx-builder/src/interfaces/IEmailTypes.sol";
+import {IDKIMRegistry} from "@zk-email/contracts/DKIMRegistry.sol";
+import {IVerifier} from "@zk-email/email-tx-builder/src/interfaces/IVerifier.sol";
+
+contract ZKEmailRegistrar {
+    using ZKEmailUtils for EmailAuthMsg;
+
+    IVerifier public verifier;
+    IDKIMRegistry public dkimregistry;
+
+    error InvalidProof(ZKEmailUtils.EmailProofError error);
+
+    constructor(address _verifier, address _dkimregistry) {
+        verifier = IVerifier(_verifier);
+        dkimregistry = IDKIMRegistry(_dkimregistry);
+    }
+
+    /// @notice Allows the owner of an email address to claim the corresponding ENS name.
+    ///         For example, the owner of "myemail[at]example.com" will claim "myemail[at]example.com.email.eth".
+    ///
+    /// @param name The email address that the user is claiming to own, e.g., "myemail@example.com".
+    /// @param authMsg The zkemail proof of ownership of the claimed email.
+    function proveAndClaim(
+        bytes memory name,
+        EmailAuthMsg memory authMsg
+    ) public {
+        ZKEmailUtils.EmailProofError result = authMsg.isValidZKEmail(
+            dkimregistry,
+            verifier
+        );
+
+        if (result != ZKEmailUtils.EmailProofError.NoError)
+            revert InvalidProof(result);
+    }
+}
---
+++ test/ZKEmailRegistrar.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ZKEmailRegistrar} from "../src/ZKEmailRegistrar.sol";
+import {EmailAuthMsgFixtures, EmailAuthMsg} from "@zk-email/email-tx-builder/test/fixtures/EmailAuthMsgFixtures.sol";
+import {Groth16Verifier} from "@zk-email/email-tx-builder/test/fixtures/Groth16Verifier.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {Verifier} from "@zk-email/email-tx-builder/src/utils/Verifier.sol";
+
+contract MockDKIMRegistry {
+    function isDKIMPublicKeyHashValid(
+        string memory,
+        bytes32
+    ) external pure returns (bool) {
+        return true;
+    }
+}
+
+contract ZKEmailRegistrarTest is Test {
+    ZKEmailRegistrar public zkEmailRegistrar;
+
+    function setUp() public {
+        zkEmailRegistrar = new ZKEmailRegistrar(
+            _deployVerifier(address(this)),
+            address(new MockDKIMRegistry())
+        );
+    }
+
+    function test_proveAndClaim_shouldClaimWithValidProof() public {
+        EmailAuthMsg memory authMsg = EmailAuthMsgFixtures.getCase1();
+        zkEmailRegistrar.proveAndClaim("thezdev1@gmail.com", authMsg);
+    }
+
+    // ==== Helpers ====
+
+    function _deployVerifier(address owner) internal returns (address) {
+        address verifierProxyAddress;
+        Verifier verifierImpl = new Verifier();
+        Groth16Verifier groth16Verifier = new Groth16Verifier();
+        console.log(
+            "Groth16Verifier deployed at: %s",
+            address(groth16Verifier)
+        );
+        verifierProxyAddress = address(
+            new ERC1967Proxy(
+                address(verifierImpl),
+                abi.encodeCall(
+                    verifierImpl.initialize,
+                    (owner, address(groth16Verifier))
+                )
+            )
+        );
+        console.log("Verifier deployed at: %s", verifierProxyAddress);
+        return verifierProxyAddress;
+    }
+}
